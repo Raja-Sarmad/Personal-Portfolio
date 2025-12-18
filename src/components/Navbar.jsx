c1c06762b64983695444775896a03f105a2203a3
CodeWithSarmad <rajasarmadaz@gmail.com>
2025-12-18 15:47:46 +0500

    added parallax background and updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
           className='ml-4'
         >
           <a 
-            href='/Sarmad_Masood_Resume.pdf' 
+            href='/Sarmad Masood CV.pdf' 
             className='px-5 py-2 border-2 border-cyan-400 text-cyan-400 rounded-lg hover:bg-cyan-400/10 transition-all duration-300 font-bold text-sm'
           >
             Resume
